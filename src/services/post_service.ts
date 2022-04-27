d3c3a00b8fc218188cbd7442d392f32d785b9633
totozunino <mathiaszunino@gmail.com>
2022-04-26 23:55:23 -0300

    feat: send notification to queue

--- src/services/post_service.ts
+++ src/services/post_service.ts
@@ -1,5 +1,6 @@
 import { Post } from "../models/post";
 import { findAll, create } from "../repositories/post_repository";
+import { connect } from "amqplib";
 
 export const getAllPosts = async (): Promise<Post[]> => {
   const posts = await findAll();
@@ -8,5 +9,16 @@ export const getAllPosts = async (): Promise<Post[]> => {
 
 export const createPost = async (post: Partial<Post>): Promise<Post> => {
   const createdPost = await create(post);
+
+  const connection = await connect("amqp://localhost");
+
+  const channel = await connection.createChannel();
+
+  await channel.assertQueue("NotificationsQueue");
+
+  const notification = JSON.stringify({ title: post.title });
+
+  channel.sendToQueue("NotificationsQueue", Buffer.from(notification));
+
   return createdPost;
 };
